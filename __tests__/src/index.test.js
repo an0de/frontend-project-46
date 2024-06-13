a315a93188a744ffe81bc73908925f54942a2f03
an0de <149331026+an0de@users.noreply.github.com>
2024-06-13 23:25:03 +0300

    rewrite src/index.test.js

--- __tests__/src/index.test.js
+++ __tests__/src/index.test.js
@@ -12,67 +12,55 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path
   .join(__dirname, '..', '..', '__fixtures__', filename);
 
-const jsonFilePath3 = getFixturePath('file3.json');
-const jsonFilePath4 = getFixturePath('file4.json');
-const yamlFilePath3 = getFixturePath('file3.yaml');
-const yamlFilePath4 = getFixturePath('file4.yaml');
-const unknownFilePath1 = getFixturePath('file1.unknown');
-const unknownFilePath2 = getFixturePath('file2.unknown');
+const makeMsg = (ext, formatName) => `diff two ${ext} files with ${formatName} output'`;
 
-const stylishOutput = fs
-  .readFileSync(path.resolve('./__fixtures__/stylish34.txt'), 'utf8')
-  .trim();
-const plainOutput = fs
-  .readFileSync(path.resolve('./__fixtures__/plain34.txt'), 'utf8')
-  .trim();
+const inputFiles = {};
+const expectedOutput = { json: {}, yaml: {}, unknown: {} };
 
-test('diff two json files with stylish output', () => {
-  const output = genDiff(jsonFilePath3, jsonFilePath4, 'stylish');
-  expect(output).toEqual(stylishOutput);
+beforeAll(() => {
+  inputFiles.json = [getFixturePath('file3.json'), getFixturePath('file4.json')];
+  inputFiles.yaml = [getFixturePath('file3.yaml'), getFixturePath('file4.yaml')];
+  inputFiles.unknown = [getFixturePath('file1.unknown'), getFixturePath('file2.unknown')];
+
+  const stylishOutput = fs
+    .readFileSync(getFixturePath('stylish34.txt'), 'utf8')
+    .trim();
+  const plainOutput = fs
+    .readFileSync(getFixturePath('plain34.txt'), 'utf8')
+    .trim();
+
+  expectedOutput.json.stylish = stylishOutput;
+  expectedOutput.json.plain = plainOutput;
+  expectedOutput.json.json = stylishOutput;
+  expectedOutput.yaml.stylish = stylishOutput;
+  expectedOutput.yaml.plain = plainOutput;
+  expectedOutput.yaml.json = stylishOutput;
+  expectedOutput.unknown.stylish = '{\n}';
+  expectedOutput.unknown.plain = '';
+  expectedOutput.unknown.json = '{\n}';
 });
 
-test('diff two json files with plain output', () => {
-  const output = genDiff(jsonFilePath3, jsonFilePath4, 'plain');
-  expect(output).toEqual(plainOutput);
-});
+[
+  ['json', 'stylish'],
+  ['json', 'plain'],
+  ['yaml', 'stylish'],
+  ['yaml', 'plain'],
+  ['unknown', 'stylish'],
+  ['unknown', 'plain'],
+].forEach(([ext, formatName]) => test(makeMsg(ext, formatName), () => {
+  const [filePath1, filePath2] = inputFiles[ext];
+  const output = genDiff(filePath1, filePath2, formatName);
+  expect(output).toEqual(expectedOutput[ext][formatName]);
+}));
 
-test('diff two json files with json output', () => {
-  const jsonOutput = genDiff(jsonFilePath3, jsonFilePath4, 'json');
+[
+  ['json', 'json'],
+  ['yaml', 'json'],
+  ['unknown', 'json'],
+].forEach(([ext, formatName]) => test(makeMsg(ext, formatName), () => {
+  const [filePath1, filePath2] = inputFiles[ext];
+  const jsonOutput = genDiff(filePath1, filePath2, formatName);
   expect(() => JSON.parse(jsonOutput)).not.toThrow();
   const output = getFormatter()(JSON.parse(jsonOutput));
-  expect(output).toEqual(stylishOutput);
-});
-
-test('diff two yaml files with stylish output', () => {
-  const output = genDiff(yamlFilePath3, yamlFilePath4, 'stylish');
-  expect(output).toEqual(stylishOutput);
-});
-
-test('diff two yaml files with plain output', () => {
-  const output = genDiff(yamlFilePath3, yamlFilePath4, 'plain');
-  expect(output).toEqual(plainOutput);
-});
-
-test('diff two yaml files with json output', () => {
-  const jsonOutput = genDiff(yamlFilePath3, yamlFilePath4, 'json');
-  expect(() => JSON.parse(jsonOutput)).not.toThrow();
-  const output = getFormatter()(JSON.parse(jsonOutput));
-  expect(output).toEqual(stylishOutput);
-});
-
-test('diff two unknown files with stylish output', () => {
-  const output = genDiff(unknownFilePath1, unknownFilePath2, 'stylish');
-  expect(output).toEqual('{\n}');
-});
-
-test('diff two unknown files with plain output', () => {
-  const output = genDiff(unknownFilePath1, unknownFilePath2, 'plain');
-  expect(output).toEqual('');
-});
-
-test('diff two unknown files with json output', () => {
-  const jsonOutput = genDiff(unknownFilePath1, unknownFilePath2, 'json');
-  expect(() => JSON.parse(jsonOutput)).not.toThrow();
-  const output = JSON.parse(jsonOutput);
-  expect(output).toHaveLength(0);
-});
+  expect(output).toEqual(expectedOutput[ext][formatName]);
+}));
